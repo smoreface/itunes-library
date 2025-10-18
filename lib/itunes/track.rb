8ff65843d325dd8579934c57bc5aee1763f583b2
smoreface <smoreface@users.noreply.github.com>
2025-10-18 00:14:36 -0700

    make it match og

--- lib/itunes/track.rb
+++ lib/itunes/track.rb
@@ -41,16 +41,20 @@ module ITunes
       self['Genre']
     end
 
-    def bpm
-      self['BPM']
-    end
-
     def year
       self['Year']
     end
 
     def composer
       self['Composer']
+    end
+
+    def season_number
+      self['Season']
+    end
+
+    def episode_number
+      self['Episode Order']
     end
 
     def date_added
@@ -81,16 +85,8 @@ module ITunes
       self['Play Count'] || 0
     end
 
-    def skip_count
-      self['Skip Count'] || 0
-    end
-
-    def duration_sec
-      self['Total Time'] ? self['Total Time'] / 1000 : nil
-    end
-
-    def duration_ms
-      self['Total Time'] ? self['Total Time'] : nil
+    def total_time
+      self['Total Time'] / 1000
     end
 
     def kind
@@ -107,10 +103,6 @@ module ITunes
     
     def artwork_count
       self['Artwork Count']
-    end
-
-    def comment
-      self['Comments']
     end
 
     def location
